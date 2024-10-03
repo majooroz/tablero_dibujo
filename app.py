60a28c7aee65e03b81c964cf4bb9938603257736
majooroz <113460933+majooroz@users.noreply.github.com>
2024-10-03 08:08:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,78 +14,72 @@ from streamlit_drawable_canvas import st_canvas
 
 Expert=" "
 profile_imgenh=" "
-    
 def encode_image_to_base64(image_path):
     try:
         with open(image_path, "rb") as image_file:
             encoded_image = base64.b64encode(image_file.read()).decode("utf-8")
             return encoded_image
     except FileNotFoundError:
-        return "Error: La imagen no se encontró en la ruta especificada."
+        return "Error: No se encontró la imagen en la ruta especificada."
 
+# Configuración de la página en Streamlit
+st.set_page_config(page_title='Dibujo Inteligente 🎨')
+st.title('Tablero de Dibujo con IA 🖼️')
 
-# Streamlit 
-st.set_page_config(page_title='Tablero Inteligente')
-st.title('Tablero Inteligente')
+# Sección de la barra lateral
 with st.sidebar:
-    st.subheader("Acerca de:")
-    st.subheader("En esta aplicación veremos la capacidad que ahora tiene una máquina de interpretar un boceto")
-st.subheader("Dibuja el boceto en el panel  y presiona el botón para analizarla")
+    st.subheader("Acerca de esta app:")
+    st.subheader("Descubre cómo la inteligencia artificial puede interpretar un boceto que dibujes en este tablero interactivo.")
+    
+st.subheader("Dibuja algo en el panel y luego presiona el botón para que la IA lo analice.")
 
-# Add canvas component
-#bg_image = st.sidebar.file_uploader("Cargar Imagen:", type=["png", "jpg"])
-# Specify canvas parameters in application
+# Parámetros del canvas
 drawing_mode = "freedraw"
-stroke_width = st.sidebar.slider('Selecciona el ancho de línea', 1, 30, 5)
-#stroke_color = '#FFFFFF' # Set background color to white
-#bg_color = '#000000'
-stroke_color = "#000000" 
+stroke_width = st.sidebar.slider('Selecciona el grosor de la línea', 1, 30, 5)
+stroke_color = st.color_picker("Elige el color del trazo", "#000000")
 bg_color = '#FFFFFF'
-#realtime_update = st.sidebar.checkbox("Update in realtime", True)
 
-
-# Create a canvas component
+# Componente de canvas para dibujo
 canvas_result = st_canvas(
-    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+    fill_color="rgba(255, 165, 0, 0.3)",  # Color de relleno con algo de opacidad
     stroke_width=stroke_width,
     stroke_color=stroke_color,
     background_color=bg_color,
     height=300,
     width=400,
-    #background_image= None #Image.open(bg_image) if bg_image else None,
     drawing_mode=drawing_mode,
     key="canvas",
 )
 
-ke = st.text_input('Ingresa tu Clave')
-#os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
+# Ingreso de clave de API de OpenAI
+ke = st.text_input('Introduce tu clave de la API de OpenAI', type="password")
 os.environ['OPENAI_API_KEY'] = ke
 
-
-# Retrieve the OpenAI API Key from secrets
+# Recuperar la clave de API
 api_key = os.environ['OPENAI_API_KEY']
 
-# Initialize the OpenAI client with the API key
-client = OpenAI(api_key=api_key)
+# Inicializar cliente de OpenAI
+client = openai
 
-analyze_button = st.button("Analiza la imagen", type="secondary")
+# Botón para analizar la imagen
+analyze_button = st.button("Analizar el dibujo con IA", type="secondary")
 
-# Check if an image has been uploaded, if the API key is available, and if the button has been pressed
+# Comprobaciones para asegurarse de que la imagen está cargada, la clave API está presente, y se ha presionado el botón
 if canvas_result.image_data is not None and api_key and analyze_button:
 
-    with st.spinner("Analizando ..."):
-        # Encode the image
+    with st.spinner("Procesando tu dibujo, por favor espera..."):
+        # Convertir la imagen del canvas a un formato adecuado
         input_numpy_array = np.array(canvas_result.image_data)
-        input_image = Image.fromarray(input_numpy_array.astype('uint8'),'RGBA')
+        input_image = Image.fromarray(input_numpy_array.astype('uint8'), 'RGBA')
         input_image.save('img.png')
         
-      # Codificar la imagen en base64
- 
+        # Codificar la imagen en base64
         base64_image = encode_image_to_base64("img.png")
-            
-        prompt_text = (f"Describe in spanish briefly the image")
-    
-      # Create the payload for the completion request
+        
+        # Texto de solicitud
+        prompt_text = (f"Describe en español brevemente la imagen")
+
+        # Crear payload para la solicitud de OpenAI
         messages = [
             {
                 "role": "user",
@@ -93,51 +87,48 @@ if canvas_result.image_data is not None and api_key and analyze_button:
                     {"type": "text", "text": prompt_text},
                     {
                         "type": "image_url",
-                        "image_url":f"data:image/png;base64,{base64_image}",
+                        "image_url": f"data:image/png;base64,{base64_image}",
                     },
                 ],
             }
         ]
-    
-        # Make the request to the OpenAI API
+
+        # Realizar la solicitud a la API de OpenAI
         try:
             full_response = ""
             message_placeholder = st.empty()
             response = openai.chat.completions.create(
-              model= "gpt-4o-mini",  #o1-preview ,gpt-4o-mini
-              messages=[
-                {
-                   "role": "user",
-                   "content": [
-                     {"type": "text", "text": prompt_text},
-                     {
-                       "type": "image_url",
-                       "image_url": {
-                         "url": f"data:image/png;base64,{base64_image}",
-                       },
-                     },
-                   ],
-                  }
-                ],
-              max_tokens=500,
-              )
-            #response.choices[0].message.content
-            if response.choices[0].message.content is not None:
-                    full_response += response.choices[0].message.content
-                    message_placeholder.markdown(full_response + "▌")
-            # Final update to placeholder after the stream ends
-            message_placeholder.markdown(full_response)
-            if Expert== profile_imgenh:
-               st.session_state.mi_respuesta= response.choices[0].message.content #full_response 
-    
-            # Display the response in the app
-            #st.write(response.choices[0])
-        except Exception as e:
-            st.error(f"An error occurred: {e}")
-else:
-    # Warnings for user action required
+                model="gpt-4o-mini",  # Puedes cambiar a otro modelo si lo prefieres
+                messages=[{
+                    "role": "user",
+                    "content": [
+                        {"type": "text", "text": prompt_text},
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:image/png;base64,{base64_image}",
+                            },
+                        },
+                    ],
+                }],
+                max_tokens=500,
+            )
 
+            # Mostrar la respuesta de la IA
+            if response.choices[0].message.content is not None:
+                full_response += response.choices[0].message.content
+                message_placeholder.markdown(full_response + "▌")
+            message_placeholder.markdown(full_response)
+
+            if Expert == profile_imgenh:
+                st.session_state.mi_respuesta = response.choices[0].message.content
+    
+        except Exception as e:
+            st.error(f"Se ha producido un error: {e}")
+
+else:
+    # Advertencias para la acción del usuario
     if not api_key:
-        st.warning("Por favor ingresa tu API key.")
+        st.warning("Por favor, ingresa tu clave API de OpenAI.")
 
 
